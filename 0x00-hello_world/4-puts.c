181463f3bdfd7c45a8ab55875227c02f057e8b87
priscillah-oss <priscillahnyamhinda@gmail.com>
2022-06-30 05:02:24 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
  *
- * * main-Entry point
+ *  * main - Entry point
  *
- * * Return: 0 if successful
+ *   *
  *
- * */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
