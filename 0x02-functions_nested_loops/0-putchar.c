3014d9e0d4807875cc80aab5b9b5700649fe3959
priscillah-oss <priscillahnyamhinda@gmail.com>
2022-07-05 11:03:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char c[] = "main";
+int i;
+
+for (i = 0; i < 9; i++)
+{
+	_putchar(c[i]);
+}
 _putchar('\n');
 return (0);
 }
