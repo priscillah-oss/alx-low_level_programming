7933f03be392387520dc9d36f20fa7c3a6e294a9
priscillah-oss <61163108+priscillah-oss@users.noreply.github.com>
2022-07-28 19:19:25 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,49 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "holberton.h"
 /**
- *string_nconcat - concat 2 string
- *@s1: string 1
- *@s2: string 2
- *@n: integer value
- *Return: pointer points to a new allocated space else NULL
+ * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
+ * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
+ * @s1: input one
+ * @s2: input two
+ * @n: s2's number of bytes
+ * Return: 0
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int s1_len, s2_len, i;
+	char *arr;
+	unsigned int i, j, co, co_2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
 
-for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-;
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
 
-for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-;
+	if (n < j)
+		j = n;
 
-str = malloc(s1_len + s2_len + 1);
+	j += i;
+	arr = malloc(sizeof(char *) * (j + 1));
 
+	if (arr == NULL)
+		return (NULL);
 
-if (n >= s2_len)
-n = s2_len;
-
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-
-for (i = 0; i < n; i++)
-str[s1_len + i] = s2[i];
-
-str[s1_len + n] = '\0';
-
-if (str == NULL)
-return (NULL);
-
-return (str);
+	for (co = 0; co < i; co++)
+		arr[co] = s1[co];
+	for (co_2 = 0; co < j; co_2++)
+	{
+		arr[co] = s2[co_2];
+		co++;
+	}
+	co++;
+	arr[co] = '\0';
+	return (arr);
 }
